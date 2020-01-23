f89a7c3e358f2478e27873afba22cc65e65b3101
Peter Bolemant <peter.bolemant@softworx.digital>
2020-01-23 04:32:55 +0100

    general update

---
+++ resources/lang/sk/communication-log.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    'column' => [
+        'model_type' => 'Typ objektu',
+        'model_id' => 'ID Objektu',
+        'sendable' => 'Typ správy',
+        'action' => 'Akcia',
+        'sender' => 'Odosielateľ',
+        'recipient' => 'Prijímateľ',
+        'subject' => 'Predmet',
+        'content' => 'Obsah',
+        'is_success' => 'Odoslané',
+        'error_description' => 'Popis chyby',
+    ],
+    'field' => [
+        'model_type' => 'Typ objektu',
+        'model_id' => 'ID Objektu',
+        'sendable_id' => 'ID Správy',
+        'sendable_type' => 'Typ správy',
+        'action' => 'Akcia',
+        'sender' => 'Odosielateľ',
+        'recipient' => 'Prijímateľ',
+        'subject' => 'Predmet',
+        'content' => 'Obsah',
+        'is_success' => 'Odoslané',
+        'error_description' => 'Popis chyby',
+    ],
+    'filter' => [
+    ],
+    'model' => [
+        'title' => [
+            'singular' => 'Záznam logu komunikácie',
+            'plural' => 'Záznamy logu komunikácie',
+        ],
+    ],
+];
