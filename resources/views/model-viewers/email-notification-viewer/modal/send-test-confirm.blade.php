5d32dc3e88b764c2ac5ac07501809a82eecc036d
Peter Bolemant <peter@softworx.digital>
2020-12-20 13:35:37 +0000

    general update

--- resources/views/model-viewers/email-notification-viewer/modal/send-test-confirm.blade.php
+++ resources/views/model-viewers/email-notification-viewer/modal/send-test-confirm.blade.php
@@ -1,8 +1,8 @@
-<div id="{{ $component->getDomId('modal-send-test', $component->getModel()->getKey()) }}" class="modal fade bs-example-modal-md" tabindex="-1" role="dialog" aria-hidden="true">
+<div id="{{ $component->getDomId('modal-send-test', $component->getModel()->getKey()) }}" class="modal fade bs-example-modal-md" tabindex="-1" role="dialog">
     <div class="modal-dialog modal-md">
         <div class="modal-content ajax-overlay">
             <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span></button>
+                <button type="button" class="close" data-dismiss="modal"><i class="fa fa-times"></i></button>
                 <h4 class="modal-title">{{ $component->translate('model.title.singular') }} <small>{{ $component->translate(sprintf('action.%s', $route_method)) }}</small></h4>
             </div>
             {!! $component->getFormComponent()->render('modal.default') !!}
